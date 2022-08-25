21b0da019ef1f1241fdd662ff2eee66526149de3
umutkeskin <ukeskinatkapsul@gmail.com>
2022-08-25 17:34:40 +0300

    udpate düzgün göründü, post yapılmıyor

--- atolye.py
+++ atolye.py
@@ -1,34 +1,42 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
+from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///borrow.db"
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class atolye(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
+    equipment = db.Column(db.String(100), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
-    date_deadline = db.Column(db.DateTime)
+    quantity = db.Column(db.Integer)
+    staff = db.Column(db.String(30), nullable = False)
+    team_name = db.Column(db.String(100), nullable = False)
+
 
     def __repr__(self):
         return "<Task %r>" % self.id
        
 
-
-
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        task_content = request.form["content"]
-        new_task = Todo(content=task_content)
-        tobe_deadline = request.form["date_deadline"]
-        gonebe_deadline = datetime.strptime(tobe_deadline, "%Y-%m-%d") 
-        new_deadline = Todo(date_deadline=gonebe_deadline,content=task_content)
+        borrow_equipment = request.form["equipment"]
+        new_equipment = atolye(equipment=borrow_equipment)
+
+        borrow_quantity = request.form["quantity"]
+        new_quantity = atolye(quantity=borrow_quantity)
+        
+        borrow_staff = request.form["staff"]
+        new_staff = atolye(quantity=borrow_staff)
+        
+        borrow_team_name = request.form["team_name"]
+        new_team_name = atolye(quantity=borrow_team_name)
+
 
         try:
-            db.session.add(new_deadline, new_task) #new_task, bunu kaldırınca niye tarih çalışıyor???
+            db.session.add(new_equipment, new_quantity, new_staff, new_team_name) #new_task, bunu kaldırınca niye tarih çalışıyor???
             #db.session.add(new_deadline)
             db.session.commit()
             return redirect("/")
@@ -36,16 +44,16 @@ def index():
             return "There is an issue adding your task"
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template("index.html", tasks = tasks)
+        borrows = atolye.query.order_by(atolye.date_created).all()
+        return render_template("index.html", borrows = borrows)
 
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    item_to_delete = atolye.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(item_to_delete)
         db.session.commit()
         return redirect("/")
     except:
@@ -53,10 +61,13 @@ def delete(id):
 
 @app.route("/update/<int:id>", methods = ["GET", "POST"])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    borrow = atolye.query.get_or_404(id)
     if request.method == "POST":
-        task.content = request.form["content"]
-        #task.date_deadline = request.form["date_deadline"]
+        borrow.equipment = request.form["equipment"]
+        borrow.quantity = request.form["quantity"]
+        borrow.staff = request.form["staff"]
+        borrow.team_name = request.form["team_name"]
+        
 
         try:
             db.session.commit()
@@ -65,7 +76,7 @@ def update(id):
             return "There was an issue updating your task"
 
     else:
-         return render_template("update.html", task = task)  
+         return render_template("update.html", borrow = borrow)  
 
 
 if __name__ == "__main__":
