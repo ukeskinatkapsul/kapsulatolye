809587d237634ff37b5af80987ae7a6bdad5dd52
umutkeskin <ukeskinatkapsul@gmail.com>
2022-08-25 12:51:22 +0300

    all cloned from todoapp and renamed

---
+++ atolye.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    date_deadline = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
+       
+
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+        tobe_deadline = request.form["date_deadline"]
+        gonebe_deadline = datetime.strptime(tobe_deadline, "%Y-%m-%d") 
+        new_deadline = Todo(date_deadline=gonebe_deadline,content=task_content)
+
+        try:
+            db.session.add(new_deadline, new_task) #new_task, bunu kaldırınca niye tarih çalışıyor???
+            #db.session.add(new_deadline)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There is an issue adding your task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks = tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task"
+
+@app.route("/update/<int:id>", methods = ["GET", "POST"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form["content"]
+        #task.date_deadline = request.form["date_deadline"]
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue updating your task"
+
+    else:
+         return render_template("update.html", task = task)  
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
